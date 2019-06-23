1217dc23abcd80591af2b550994788601821c2c1
Jonathan-Can Kleiner <canozbek1@yahoo.com>
2019-06-23 10:06:21 +0200

    Observer Pattern

--- src/main/java/com/greydev/design_patterns/App.java
+++ src/main/java/com/greydev/design_patterns/App.java
@@ -1,5 +1,8 @@
 package com.greydev.design_patterns;
 
+import com.greydev.design_patterns.observer.weatherstation.CarDisplay;
+import com.greydev.design_patterns.observer.weatherstation.PhoneDisplay;
+import com.greydev.design_patterns.observer.weatherstation.WeatherStation;
 import com.greydev.design_patterns.strategy.brake.SUV;
 import com.greydev.design_patterns.strategy.brake.Sedan;
 import com.greydev.design_patterns.strategy.sort.BubbleSort;
@@ -8,10 +11,34 @@ import com.greydev.design_patterns.strategy.sort.MyCollection;
 public class App {
 
 	public static void main(String[] args) {
-		startStrategyPatternDemo();
+
+		strategyPatternDemo();
+
+		observerPatternDemo();
+
 	}
 
-	public static void startStrategyPatternDemo() {
+	private static void observerPatternDemo() {
+		System.out.println("----- Observer Pattern -----");
+
+		WeatherStation weatherStation = new WeatherStation();
+		weatherStation.setCurrentTemp(15);
+
+		CarDisplay carDisplay = new CarDisplay(weatherStation);
+		carDisplay.showCurrentTemparature();
+		PhoneDisplay phoneDisplay = new PhoneDisplay(weatherStation);
+		phoneDisplay.showCurrentTemparature();
+
+		System.out.println("*** temp is updated ***");
+		weatherStation.setCurrentTemp(20);
+		carDisplay.showCurrentTemparature();
+		phoneDisplay.showCurrentTemparature();
+		System.out.println();
+	}
+
+	public static void strategyPatternDemo() {
+		System.out.println("----- Strategy Pattern -----");
+
 		MyCollection collectionDefaultSorting = new MyCollection();
 		collectionDefaultSorting.sortString("blabla");
 
@@ -23,5 +50,6 @@ public class App {
 
 		SUV suv = new SUV();
 		suv.applyBrake();
+		System.out.println();
 	}
 }
